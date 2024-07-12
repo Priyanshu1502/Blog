50d9cb0f585ab703ea963658fc8add9260302cb0
Priyanshu1502 <priyanshubhatt1502@gmail.com>
2024-07-12 08:00:40 +0530

    all states in redux are working in signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import DashBoard from "./pages/Dashboard"
-import Project from "./pages/Projects"
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/Dashboard";
+import Project from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route  path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path='/dashboard' element={<DashBoard />} />
-        <Route path='/projects' element= {<Project />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/projects" element={<Project />} />
       </Routes>
-    <Footer />
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,108 +1,121 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {
+  signInStart,
+  signInFaliure,
+  signInSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignIn = () => {
-
-  const [formData, setFormData] = useState({})
-  const [errorMessage,setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleChange = (e)=>{
-      setFormData({
-        ...formData, [e.target.id] : e.target.value.trim()
-      })}
-    
-  const handleSubmit = async (e)=>{
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill out all the fields')
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFaliure("All fields are required"));
     }
-    try{
-      setLoading(true)
-      setErrorMessage(null)
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-      const data = await res.json()
-      if(data.success === false){
-        return setErrorMessage(data.message)
-      }setLoading(false)
-      if(res.ok){
-        navigate('/home')
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFaliure(data.message));
       }
-    }catch(error){
-      setErrorMessage(error.message)
-      setLoading(false)
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFaliure(error.message));
+      // console.log("Error while auth");
     }
-  }
+  };
 
   return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/*left*/}
-        <div className='flex-1'>
-        <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Simply</span>
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Simply
+            </span>
             Blog
-        </Link>
-        <p className='text-sm mt-5'>
-        This is a demo website, to continue with the blog please Sign in to  an account.
-        </p>
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo website, to continue with the blog please Sign in to
+            an account.
+          </p>
         </div>
         {/*right*/}
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value='E-mail' />
+              <Label value="E-mail" />
               <TextInput
-              type='email'
-              placeholder='someone123@email.com'
-              id='email' 
+                type="email"
+                placeholder="someone123@email.com"
+                id="email"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Password' />
+              <Label value="Password" />
               <TextInput
-              type='password'
-              placeholder='Password'
-              id='password' 
+                type="password"
+                placeholder="Password"
+                id="password"
                 onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
-              {
-                loading ? (
-                  <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
-                  </>
-                )
-                 : 'Sign In'
-              }
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
             </Button>
           </form>
-          <div className='flex gap-2 text-sm mt-5'>
+          <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an account?</span>
-            <Link to='sign-in' className='text-blue-500' >
+            <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
           </div>
-          {
-            errorMessage && (
-              <Alert className='mt-5' color='failure'>
-                {errorMessage}
-              </Alert>
-            )
-          }
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
