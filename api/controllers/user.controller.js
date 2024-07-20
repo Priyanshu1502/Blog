63572e96aaa36eb0b5bbd098c205ad491fbfb4ae
Priyanshu1502 <priyanshubhatt1502@gmail.com>
2024-07-20 22:02:39 +0530

    complete update user profile page functonality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -35,23 +35,23 @@ export const updateUser = async (req, res, next) => {
         errorHandeler(400, "Username must contain only letters and numbers")
       );
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            profilePicture: req.body.profilePicture,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
